f15817298e0b63916dfcee57641cca9c823b88f2
ashscripting <arshia.mortazavinezhad@ontariotechu.net>
2024-02-16 16:14:35 -0500

    initial report commit

---
+++ cmake-build-debug/CMakeFiles/Algorithm_Assignment_1.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "Algorithm_Assignment_1.exe"
+  "Algorithm_Assignment_1.exe.manifest"
+  "Algorithm_Assignment_1.pdb"
+  "CMakeFiles/Algorithm_Assignment_1.dir/file_handler.cpp.obj"
+  "CMakeFiles/Algorithm_Assignment_1.dir/file_handler.cpp.obj.d"
+  "CMakeFiles/Algorithm_Assignment_1.dir/main.cpp.obj"
+  "CMakeFiles/Algorithm_Assignment_1.dir/main.cpp.obj.d"
+  "libAlgorithm_Assignment_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Algorithm_Assignment_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
